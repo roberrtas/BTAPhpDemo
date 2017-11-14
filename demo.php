56e41348304d0104369ed07ff8bbc0266115c0f1
unknown <alisauskas.robertas@gmail.com>
2017-11-14 18:33:04 +0200

    add demo file

---
+++ demo.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>helllo</h1> -->
+    <?php
+        // $name = "robertas";
+        // echo $name . " ";
+
+        // $a = 8;
+        
+        // $a++;
+        
+        // ++$a;
+        
+        // $x = $a-- + 2;
+        
+        // $y = ++$a * 2;
+        
+        // $z = $a++ + ++$a +$a;
+        
+        // echo $z;
+
+        // $c = "HI $z labas";
+        // echo $c;
+
+        // $gt = htmlentities("<b>$a</b>");
+        // $gg = html_entity_decode("<b>$a</b>");
+        // echo $gt;
+        // echo $gg;
+
+
+        $aa = array(2, 4, 8, 16);
+        
+        $xx = $aa[3];
+        
+        $bb = array(array(2, 4), array(3, 9), array(5, 25));
+        var_dump($bb);
+        $yy = $bb[2][1];
+
+     ?>
+</body>
+</html>
